180766026393463e3fe541adeafba02af3955825
EdgarMz-U <L22550330@chihuahua2.tecnm.mx>
2025-10-28 19:50:40 -0600

    Add files via upload

---
+++ EVA3_1_FUNCIONES_1.sql
@@ -0,0 +1,5 @@
+use tbd3;
+
+create table prueba(
+id int not null key auto_increment,
+nombre varchar(50));
---
+++ EVA3_1_FUNCIONES_6.sql
@@ -0,0 +1,16 @@
+delimiter $$
+
+create function comparar (val1 int, val2 int)
+returns varchar(50) deterministic
+begin 
+	declare resu varchar(50);
+    if val1 < val2 then
+		set resu = "Valor 2 MAYOR que Valor 1";
+	elseif val1 > val2 then
+		set resu = "Valor 1 MAYOR que Valor 2";
+	else
+		set resu = "Ambos valores son IGUALES";
+    end if;
+    return resu;
+end$$
+delimiter ;
