edeb8a2cc218ec397890bc9ce38a96709ef9627a
EdgarMz-U <L22550330@chihuahua2.tecnm.mx>
2025-11-06 19:52:35 -0600

    Add files via upload

---
+++ EVA3_5_CURSOR_2.sql
@@ -0,0 +1,29 @@
+delimiter $$
+create procedure rfc_actores()
+begin
+	declare id int;
+    declare nombre varchar(50);
+    declare apellido varchar(50);
+    declare fecha date;
+	declare terminar int default false;
+	declare contar_actores cursor for 
+    select actor_id, first_name, last_name, last_update
+    from actor;
+    declare continue handler for not found set terminar = true;
+    
+    open contar_actores;
+    
+    mi_cursor: loop
+		fetch contar_actores into id, nombre, apellido, fecha;
+        if terminar then
+			leave mi_cursor;
+        end if;
+    end loop;
+    
+    close contar_actores;
+    
+    select generar_rfc(first_name, last_name, "", date(last_update)) as rfc_actores from actor;
+
+end$$
+
+delimiter ;
